7b7592508cdb8c4e0338125d1c3150e63f3371d3
assessoriajhessiimoura-gif <assessoriajhessiimoura@gmail.com>
2025-12-06 16:30:10 -0300

    Update image path for Mega Volume item

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -34,7 +34,7 @@ const procedures: Procedure[] = [
   },
   {
     name: 'Mega Volume',
-    image: '/images/whatsapp_image_2025-11-29_at_20.21.38_(1).jpeg',
+    image: 'whatsapp_image_2025-11-29_at_20.21.39.jpeg',
     price: 'R$ 220,00',
   },
 ];
