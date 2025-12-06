4d248bb3f8fa26819e66d1d574f163edd77f840c
assessoriajhessiimoura-gif <assessoriajhessiimoura@gmail.com>
2025-12-06 16:29:13 -0300

    Update image filename for Volume Europeu

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -24,7 +24,7 @@ const procedures: Procedure[] = [
   },
   {
     name: 'Volume Europeu',
-    image: '/images/whatsapp_image_2025-11-29_at_20.21.39.jpeg',
+    image: 'whatsapp_image_2025-11-29_at_20.21.38_(2).jpeg',
     price: 'R$ 165,00',
   },
   {
